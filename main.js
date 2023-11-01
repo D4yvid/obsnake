c4c18b6887c6b3fb3d61e31446a92bff14b702cc
Dayvid Albuquerque <kkd4yv1d@gmail.com>
2023-11-01 17:22:28 -0300

    Fix tabs

--- main.js
+++ main.js
@@ -12,124 +12,124 @@ const TILE_SIZE = 32;
  * @param {HTMLCanvasElement} canvas
  */
 function createGame(canvas) {
-	const ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d');
 
-	const width = canvas.width;
-	const height = canvas.height;
+  const width = canvas.width;
+  const height = canvas.height;
 
-	const UP = { x: 0, y: -1 };
-	const DOWN = { x: 0, y: 1 };
-	const LEFT = { x: -1, y: 0 };
-	const RIGHT = { x: 1, y: 0 };
+  const UP = { x: 0, y: -1 };
+  const DOWN = { x: 0, y: 1 };
+  const LEFT = { x: -1, y: 0 };
+  const RIGHT = { x: 1, y: 0 };
 
-	const tilesWidth = Math.floor(width / TILE_SIZE);
-	const tilesHeight = Math.floor(height / TILE_SIZE);
+  const tilesWidth = Math.floor(width / TILE_SIZE);
+  const tilesHeight = Math.floor(height / TILE_SIZE);
 
-	let running = false;
+  let running = false;
 
-	let snake = {
-		dx: 1,
-		dy: 0,
-		x: 0,
-		y: 0,
-		size: 3,
+  let snake = {
+    dx: 1,
+    dy: 0,
+    x: 0,
+    y: 0,
+    size: 3,
 
-		/** @type {{x:number,y:number}[]} tail */
-		tail: []
-	};
+    /** @type {{x:number,y:number}[]} tail */
+    tail: []
+  };
 
-	let apple = generateApple();
+  let apple = generateApple();
 
-	function generateApple() {
-		return {
-			x: Math.floor(Math.random() * tilesWidth),
-			y: Math.floor(Math.random() * tilesHeight)
-		}
-	}
+  function generateApple() {
+    return {
+      x: Math.floor(Math.random() * tilesWidth),
+      y: Math.floor(Math.random() * tilesHeight)
+    }
+  }
 
-	function move(direction) {
-		if (snake.dx == -direction.x || snake.dy == -direction.y) {
-			return;
-		}
+  function move(direction) {
+    if (snake.dx == -direction.x || snake.dy == -direction.y) {
+      return;
+    }
 
-		snake.dx = direction.x;
-		snake.dy = direction.y;
-	}
+    snake.dx = direction.x;
+    snake.dy = direction.y;
+  }
 
-	function start() {
-		running = true;
-	}
+  function start() {
+    running = true;
+  }
 
-	function stop() {
-		running = false;
-	}
+  function stop() {
+    running = false;
+  }
 
-	function update() {
-		snake.x += snake.dx;
-		snake.y += snake.dy;
+  function update() {
+    snake.x += snake.dx;
+    snake.y += snake.dy;
 
-		if (snake.x >= tilesWidth) {
-			snake.x = 0;
-		} else if (snake.x < 0) {
-			snake.x = tilesWidth - 1;
-		}
+    if (snake.x >= tilesWidth) {
+      snake.x = 0;
+    } else if (snake.x < 0) {
+      snake.x = tilesWidth - 1;
+    }
 
-		if (snake.y > tilesHeight) {
-			snake.y = 0;
-		} else if (snake.y < 0) {
-			snake.y = tilesHeight;
-		}
+    if (snake.y > tilesHeight) {
+      snake.y = 0;
+    } else if (snake.y < 0) {
+      snake.y = tilesHeight;
+    }
 
-		if (snake.x == apple.x && snake.y == apple.y) {
-			snake.size++;
-			apple = generateApple();
-		}
+    if (snake.x == apple.x && snake.y == apple.y) {
+      snake.size++;
+      apple = generateApple();
+    }
 
-		let head = { x: snake.x, y: snake.y };
+    let head = { x: snake.x, y: snake.y };
 
-		snake.tail.push(head);
+    snake.tail.push(head);
 
-		while (snake.tail.length > snake.size)
-			snake.tail.shift();
-	}
+    while (snake.tail.length > snake.size)
+      snake.tail.shift();
+  }
 
-	function render() {
-		ctx.fillStyle = 'black';
-		ctx.fillRect(0, 0, width, height);
+  function render() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, width, height);
 
-		ctx.fillStyle = 'red';
-		ctx.fillRect(apple.x * TILE_SIZE, apple.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(apple.x * TILE_SIZE, apple.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 
-		for (const tail of snake.tail) {
-			ctx.fillStyle = 'green';
+    for (const tail of snake.tail) {
+      ctx.fillStyle = 'green';
 
-			ctx.fillRect(tail.x * TILE_SIZE, tail.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-		}
-	}
+      ctx.fillRect(tail.x * TILE_SIZE, tail.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    }
+  }
 
-	/**
-	 * @param {KeyboardEvent} event
-	 */
-	function onKeyDown(event) {
-		switch (event.key) {
-			case 'ArrowUp': move(UP); break;
-			case 'ArrowDown': move(DOWN); break;
-			case 'ArrowLeft': move(LEFT); break;
-			case 'ArrowRight': move(RIGHT); break;
-		}
-	}
+  /**
+   * @param {KeyboardEvent} event
+   */
+  function onKeyDown(event) {
+    switch (event.key) {
+      case 'ArrowUp': move(UP); break;
+      case 'ArrowDown': move(DOWN); break;
+      case 'ArrowLeft': move(LEFT); break;
+      case 'ArrowRight': move(RIGHT); break;
+    }
+  }
 
-	function tick() {
-		if (!running) {
-			games.remove(this);
-			return;
-		}
+  function tick() {
+    if (!running) {
+      games.remove(this);
+      return;
+    }
 
-		update();
-		render();
-	}
+    update();
+    render();
+  }
 
-	return { start, stop, onKeyDown, tick };
+  return { start, stop, onKeyDown, tick };
 }
 
 
@@ -143,7 +143,7 @@ class SnakeView extends View {
     super(leaf);
 
     this.plugin = plugin;
-		this.gameInstance = null;
+    this.gameInstance = null;
   }
 
   getDisplayText() {
@@ -155,27 +155,27 @@ class SnakeView extends View {
   }
 
   async onOpen() {
-		try {
-			const width  = this.containerEl.parentElement.parentElement.offsetWidth;
-			const height = this.containerEl.parentElement.parentElement.offsetHeight;
+    try {
+      const width  = this.containerEl.parentElement.parentElement.offsetWidth;
+      const height = this.containerEl.parentElement.parentElement.offsetHeight;
 
-			const canvas = this.containerEl.createEl('canvas');
+      const canvas = this.containerEl.createEl('canvas');
 
-			canvas.width = width;
-			canvas.height = height;
+      canvas.width = width;
+      canvas.height = height;
 
-			this.containerEl.appendChild(canvas);
+      this.containerEl.appendChild(canvas);
 
-			games.push(this.gameInstance = createGame(canvas));
+      games.push(this.gameInstance = createGame(canvas));
 
-			this.gameInstance.start();
-		} catch (error) {
-			new Notice('Error: ' + error);
-		}
+      this.gameInstance.start();
+    } catch (error) {
+      new Notice('Error: ' + error);
+    }
   }
 
   async onClose() {
-		this.gameInstance.stop();
+    this.gameInstance.stop();
   }
 
 }
@@ -186,42 +186,42 @@ module.exports = class SnakePlugin extends Plugin {
     this.registerView(...this.snakeView());
     this.addCommand(this.snakeCommand());
 
-		this.registerInterval(setInterval(() => {
-			for (const game of games) {
-				game.tick();
-			}
-		}, 1000 / 12));
+    this.registerInterval(setInterval(() => {
+      for (const game of games) {
+        game.tick();
+      }
+    }, 1000 / 12));
 
-		this.registerDomEvent(window, 'keydown', (keydown) => {
-			for (const game of games) {
-				game.onKeyDown(keydown);
-			}
-		});
+    this.registerDomEvent(window, 'keydown', (keydown) => {
+      for (const game of games) {
+        game.onKeyDown(keydown);
+      }
+    });
   }
 
-	snakeView = () => [
-		SNAKE_VIEW,
-		(leaf) => new SnakeView(leaf, this)
-	];
+  snakeView = () => [
+    SNAKE_VIEW,
+    (leaf) => new SnakeView(leaf, this)
+  ];
 
-	snakeCommand = () => ({
-		id: 'play-snake',
-		name: 'Play Snake',
+  snakeCommand = () => ({
+    id: 'play-snake',
+    name: 'Play Snake',
 
-		callback: async () => {
-			let leaves = this.app.workspace.getLeavesOfType(SNAKE_VIEW);
-			let leaf = null;
+    callback: async () => {
+      let leaves = this.app.workspace.getLeavesOfType(SNAKE_VIEW);
+      let leaf = null;
 
-			if (leaves.length > 0) {
-				leaf = leaves[0];
-			} else {
-				leaf = this.app.workspace.getLeaf(true);
+      if (leaves.length > 0) {
+        leaf = leaves[0];
+      } else {
+        leaf = this.app.workspace.getLeaf(true);
 
-				await leaf.setViewState({ type: SNAKE_VIEW, active: true });
-			}
+        await leaf.setViewState({ type: SNAKE_VIEW, active: true });
+      }
 
-			this.app.workspace.revealLeaf(leaf);
-		}
-	});
+      this.app.workspace.revealLeaf(leaf);
+    }
+  });
 
 }
